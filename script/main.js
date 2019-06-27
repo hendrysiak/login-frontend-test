bb2faa39cfa1d8e22bc881bd6222b8fea438442a
hendrysiak <44636585+hendrysiak@users.noreply.github.com>
2019-06-27 20:27:20 +0200

    Fixed bugs

--- script/main.js
+++ script/main.js
@@ -1,6 +1,7 @@
 const form = document.querySelector('form');
+const formContent = [...form.children]
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    form.innerHTML = ''
+    formContent.forEach(content => content.classList.add('hidden'))
 })
