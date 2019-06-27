690ccb8325cc9213172d9067e087cfa1ca91af40
hendrysiak <44636585+hendrysiak@users.noreply.github.com>
2019-06-27 22:18:50 +0200

    Fixed names of variables

--- script/main.js
+++ script/main.js
@@ -1,7 +1,7 @@
 const form = document.querySelector('form');
 const formContent = [...form.children]
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
     formContent.forEach(content => content.classList.add('hidden'))
 })
